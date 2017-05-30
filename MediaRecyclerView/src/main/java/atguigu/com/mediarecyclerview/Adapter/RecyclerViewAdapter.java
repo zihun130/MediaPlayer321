b00d4909a8a863d1953181f0635c2f68c29fd0d5
zihun130 <zihun130@126.com>
2017-05-30 15:24:10 +0800

    创建各个Viewholder并重写onBindViewHolder

--- MediaRecyclerView/src/main/java/atguigu/com/mediarecyclerview/Adapter/RecyclerViewAdapter.java
+++ MediaRecyclerView/src/main/java/atguigu/com/mediarecyclerview/Adapter/RecyclerViewAdapter.java
@@ -4,18 +4,25 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import org.xutils.common.util.DensityUtil;
+import org.xutils.image.ImageOptions;
 import org.xutils.x;
 
 import java.util.List;
 
 import atguigu.com.mediarecyclerview.R;
 import atguigu.com.mediarecyclerview.domain.RecyclerViewInfo;
-
-import static android.media.tv.TvTrackInfo.TYPE_VIDEO;
+import atguigu.com.mediarecyclerview.utils.Utils;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
 /**
  * Created by sun on 2017/5/30.
@@ -45,7 +52,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter {
         switch (itemviewType) {
             case TYPE_RECYCLER_VIDEO :
                 convertView = View.inflate(context, R.layout.videoholder_item, null);
-                viewHolder = new VideoHoder(convertView);
+                viewHolder = new VideoHolder(convertView);
 
                 break;
             case TYPE_RECYCLER_IMAGE :
@@ -76,20 +83,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
         if (getItemViewType(position) == TYPE_RECYCLER_VIDEO) {
-            VideoHoder videoHoder = (VideoHoder) holder;
-            videoHoder.setData(datas.get(position));
+            VideoHolder videoHoder = (VideoHolder) viewHolder;
+            videoHoder.setData(list.get(position));
         } else if (getItemViewType(position) == TYPE_RECYCLER_IMAGE) {
-            ImageHolder imageHolder = (ImageHolder) holder;
-            imageHolder.setData(datas.get(position));
+            ImageHolder imageHolder = (ImageHolder) viewHolder;
+            imageHolder.setData(list.get(position));
         } else if (getItemViewType(position) == TYPE_RECYCLER_TEXT) {
-            TextHolder textHolder = (TextHolder) holder;
-            textHolder.setData(datas.get(position));
+            TextHolder textHolder = (TextHolder) viewHolder;
+            textHolder.setData(list.get(position));
         } else if (getItemViewType(position) == TYPE_RECYCLER_GIF) {
-            GifHolder gifHolder = (GifHolder) holder;
-            gifHolder.setData(datas.get(position));
+            GifHolder gifHolder = (GifHolder) viewHolder;
+            gifHolder.setData(list.get(position));
         } else {
-            ADHolder adHolder = (ADHolder) holder;
-            adHolder.setData(datas.get(position));
+            HtmlHolder htmlHolder = (HtmlHolder) viewHolder;
+            htmlHolder.setData(list.get(position));
         }
 
     }
@@ -154,6 +161,133 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter {
     }
 
 
+    class VideoHolder extends BaseViewHolder {
+        Utils utils;
+        TextView tvContext;
+        JCVideoPlayerStandard jcvVideoplayer;
+        TextView tvPlayNums;
+        TextView tvVideoDuration;
+        ImageView ivCommant;
+        TextView tvCommantContext;
 
+
+        public VideoHolder(View itemView) {
+            super(itemView);
+
+            tvContext = (TextView) itemView.findViewById(R.id.tv_context);
+            utils = new Utils();
+            tvPlayNums = (TextView) itemView.findViewById(R.id.tv_play_nums);
+            tvVideoDuration = (TextView) itemView.findViewById(R.id.tv_video_duration);
+            ivCommant = (ImageView) itemView.findViewById(R.id.iv_commant);
+            tvCommantContext = (TextView) itemView.findViewById(R.id.tv_commant_context);
+            jcvVideoplayer = (JCVideoPlayerStandard) itemView.findViewById(R.id.jcv_videoplayer);
+
+        }
+
+        public void setData(RecyclerViewInfo.ListBean mediaItem) {
+            super.setData(mediaItem);
+
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
+            //第一个参数是视频播放地址，第二个参数是显示封面的地址，第三参数是标题
+            boolean setUp = jcvVideoplayer.setUp(
+                    mediaItem.getVideo().getVideo().get(0), JCVideoPlayer.SCREEN_LAYOUT_LIST,
+                    "");
+
+            if (setUp) {
+            Glide.with(context).load(mediaItem.getVideo().getThumbnail().get(0)).into(jcvVideoplayer.thumbImageView);
+            }
+            tvPlayNums.setText(mediaItem.getVideo().getPlaycount() + "次播放");
+            tvVideoDuration.setText(utils.stringForTime(mediaItem.getVideo().getDuration() * 1000) + "");
+
+        }
+    }
+
+    class ImageHolder extends BaseViewHolder {
+        TextView tvContext;
+        ImageView ivImageIcon;
+
+        public ImageHolder(View itemView) {
+            super(itemView);
+            tvContext = (TextView) itemView.findViewById(R.id.tv_context);
+            ivImageIcon = (ImageView) itemView.findViewById(R.id.iv_image_icon);
+        }
+
+        public void setData(RecyclerViewInfo.ListBean mediaItem) {
+            super.setData(mediaItem);
+
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
+           //图片特有的
+            ivImageIcon.setImageResource(R.drawable.bg_item);
+            if (mediaItem.getImage() != null && mediaItem.getImage() != null && mediaItem.getImage().getSmall() != null) {
+                Glide.with(context).load(mediaItem.getImage().getDownload_url().get(0)).placeholder(R.drawable.bg_item).error(R.drawable.bg_item).diskCacheStrategy(DiskCacheStrategy.ALL).into(ivImageIcon);
+            }
+
+        }
+    }
+
+
+    class TextHolder extends BaseViewHolder {
+        TextView tvContext;
+
+        public TextHolder(View itemView) {
+            super(itemView);
+            tvContext = (TextView) itemView.findViewById(R.id.tv_context);
+        }
+
+        public void setData(RecyclerViewInfo.ListBean mediaItem) {
+            super.setData(mediaItem);
+//设置文本-所有的都有
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
+        }
+
+    }
+
+
+    class GifHolder extends BaseViewHolder {
+        TextView tvContext;
+        ImageView ivImageGif;
+        private ImageOptions imageOptions;
+
+        public GifHolder(View itemView) {
+            super(itemView);
+            tvContext = (TextView) itemView.findViewById(R.id.tv_context);
+            imageOptions = new ImageOptions.Builder()
+                    .setSize(ViewGroup.LayoutParams.WRAP_CONTENT, -2)
+//设置圆角
+                    .setRadius(DensityUtil.dip2px(5))
+                    .setIgnoreGif(false)//是否忽略gif图。false表示不忽略。不写这句，默认是true
+                    .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                    .setLoadingDrawableId(R.drawable.video_default)
+                    .setFailureDrawableId(R.drawable.video_default)
+                    .build();
+
+        }
+
+        public void setData(RecyclerViewInfo.ListBean mediaItem) {
+            super.setData(mediaItem);
+
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
+            if (mediaItem.getGif() != null && mediaItem.getGif() != null && mediaItem.getGif().getImages() != null) {
+//                Glide.with(context).load(mediaItem.getGif().getImages().get(0)).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(ivImageGif);
+                x.image().bind(ivImageGif, mediaItem.getGif().getImages().get(0), imageOptions);
+            }
+
+        }
+    }
+
+    class HtmlHolder extends BaseViewHolder {
+        TextView tvContext;
+        ImageView ivImageIcon;
+        Button btnInstall;
+
+        public HtmlHolder(View itemView) {
+            super(itemView);
+
+            tvContext = (TextView) itemView.findViewById(R.id.tv_context);
+            btnInstall = (Button) itemView.findViewById(R.id.btn_install);
+            ivImageIcon = (ImageView) itemView.findViewById(R.id.iv_image_icon);
+
+        }
+    }
 
 }
---
+++ MediaRecyclerView/src/main/java/atguigu/com/mediarecyclerview/utils/Utils.java
@@ -0,0 +1,66 @@
+package atguigu.com.mediarecyclerview.utils;
+
+import android.content.Context;
+import android.net.TrafficStats;
+
+import java.util.Formatter;
+import java.util.Locale;
+
+public class Utils {
+
+	private StringBuilder mFormatBuilder;
+	private Formatter mFormatter;
+	private long lastTotalRxBytes = 0;
+	private long lastTimeStamp = 0;
+
+	public Utils() {
+		// 转换成字符串的时间
+		mFormatBuilder = new StringBuilder();
+		mFormatter = new Formatter(mFormatBuilder, Locale.getDefault());
+
+	}
+
+	/**
+	 * 把毫秒转换成：1:20:30这里形式
+	 * @param timeMs
+	 * @return
+	 */
+	public String stringForTime(int timeMs) {
+		int totalSeconds = timeMs / 1000;
+		int seconds = totalSeconds % 60;
+
+		int minutes = (totalSeconds / 60) % 60;
+
+		int hours = totalSeconds / 3600;
+
+		mFormatBuilder.setLength(0);
+		if (hours > 0) {
+			return mFormatter.format("%d:%02d:%02d", hours, minutes, seconds)
+					.toString();
+		} else {
+			return mFormatter.format("%02d:%02d", minutes, seconds).toString();
+		}
+	}
+
+	public boolean isNetUri(String data){
+		boolean isNetUri=false;
+		if(data!=null){
+			if(data.toLowerCase().startsWith("http") || data.toLowerCase().startsWith("mms") || data.toLowerCase().startsWith("rtsp"));
+			isNetUri=true;
+		}
+
+		return isNetUri;
+	}
+
+
+     // 得到当前的网
+	public String getNetSpeed(Context context) {
+		long nowTotalRxBytes = TrafficStats.getUidRxBytes(context.getApplicationInfo().uid)==TrafficStats.UNSUPPORTED ? 0 :(TrafficStats.getTotalRxBytes()/1024);//转为KB;
+		long nowTimeStamp = System.currentTimeMillis();
+		long speed = ((nowTotalRxBytes - lastTotalRxBytes) * 1000 / (nowTimeStamp - lastTimeStamp));//毫秒转换
+		lastTimeStamp = nowTimeStamp;
+		lastTotalRxBytes = nowTotalRxBytes;
+		String msg  = String.valueOf(speed) + " kb/s";
+		return msg;
+	}
+}
