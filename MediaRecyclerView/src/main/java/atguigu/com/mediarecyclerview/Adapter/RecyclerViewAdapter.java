45e7b1e15f02fadac8530dbc08beb5b034ceb5f6
zihun130 <zihun130@126.com>
2017-05-30 14:20:27 +0800

    重写onCreatViewHolder方法

--- MediaRecyclerView/src/main/java/atguigu/com/mediarecyclerview/Adapter/RecyclerViewAdapter.java
+++ MediaRecyclerView/src/main/java/atguigu/com/mediarecyclerview/Adapter/RecyclerViewAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import java.util.List;
 
+import atguigu.com.mediarecyclerview.R;
 import atguigu.com.mediarecyclerview.domain.RecyclerViewInfo;
 
 /**
@@ -15,6 +16,10 @@ import atguigu.com.mediarecyclerview.domain.RecyclerViewInfo;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter {
     private static final int TYPE_RECYCLER_VIDEO = 0;
+    private static final int TYPE_RECYCLER_IMAGE = 1;
+    private static final int TYPE_RECYCLER_TEXT = 2;
+    private static final int TYPE_RECYCLER_GIF = 3;
+    private static final int TYPE_RECYCLER_HTML = 4;
     private Context context;
     private List<RecyclerViewInfo.ListBean> list;
     public RecyclerViewAdapter(Context context, List<RecyclerViewInfo.ListBean> list) {
@@ -30,6 +35,33 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter {
     private RecyclerView.ViewHolder initViewHolder(int itemviewType) {
         RecyclerView.ViewHolder   viewHolder=null;
         View convertView = null;
+        switch (itemviewType) {
+            case TYPE_RECYCLER_VIDEO :
+                convertView = View.inflate(context, R.layout.videoholder_item, null);
+                viewHolder = new VideoHoder(convertView);
+
+                break;
+            case TYPE_RECYCLER_IMAGE :
+                convertView = View.inflate(context, R.layout.imageholder_item, null);
+                viewHolder = new ImageHolder(convertView);
+
+                break;
+            case TYPE_RECYCLER_TEXT :
+                convertView = View.inflate(context, R.layout.textholder_item, null);
+                viewHolder = new TextHolder(convertView);
+
+                break;
+            case TYPE_RECYCLER_GIF :
+                convertView = View.inflate(context, R.layout.gifholder_item, null);
+                viewHolder = new GifHolder(convertView);
+
+                break;
+            case TYPE_RECYCLER_HTML :
+                convertView = View.inflate(context, R.layout.htmlholder_item, null);
+                viewHolder = new HtmlHolder(convertView);
+
+                break;
+        }
 
         return null;
     }
